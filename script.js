795371172544e3d7cb37bba0759f94beb0865011
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2023-01-17 12:06:24 +0100

    feat: change title with lightswitch

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const button = document.querySelector("button");
 function lightswitch() {
   button.classList.toggle("button-dark");
   document.body.classList.toggle("button-dark");
+
+  if (document.title === "Good Night") {
+    document.title = "Good Morning";
+  } else {
+    document.title = "Good Night";
+  }
 }
 
 button.addEventListener("click", lightswitch);
